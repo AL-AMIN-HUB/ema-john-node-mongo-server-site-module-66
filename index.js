72ce9b0431c252d884fc0062ef0e0f25099b6669
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-11-01 19:10:36 +0600

    added JWT

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+var admin = require("firebase-admin");
 require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// firebase admin initialization
+
+var serviceAccount = require("./ema-john-simple-auth-88e5a-firebase-adminsdk-m49pu-db032d238d.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // middleware use
 app.use(cors());
@@ -13,6 +22,20 @@ app.use(express.json());
 // connected database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4xkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// verify authorization
+
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const idToken = req.headers.authorization.split("Bearer ")[1];
+
+    try {
+      const decodedUser = await admin.auth().verifyToken(idToken);
+      req.decodedUserEmail = decodedUser.email;
+    } catch {}
+  }
+  next();
+}
 
 async function run() {
   try {
@@ -54,8 +77,22 @@ async function run() {
     });
 
     //add orders Api
+    app.get("/orders", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      if (req.decodedUserEmail === email) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.json(orders);
+      } else {
+        res.status(401).json({ message: "User not Authorized" });
+      }
+    });
+
+    //
     app.post("/orders", async (req, res) => {
       const order = req.body;
+      order.createdAt = new Date();
       const result = await orderCollection.insertOne(order);
       res.json(result);
     });
